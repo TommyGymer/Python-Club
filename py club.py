54a768b426a472f3a0e0ccf47e3654ef69e40658
TommyGymer <tommygymer@gmail.com>
2022-03-23 14:39:44 +0000

    Basics

---
+++ py club.py
@@ -0,0 +1,43 @@
+# Getting started
+## Basic functions
+print()
+
+input()
+
+### Use of the basic functions
+print(input(": "))
+
+## Variables
+a = 5
+b = "string"
+c = False
+
+print(a)
+d = input(f"{b}: ")
+
+## Branching
+if c:
+    print(d)
+else:
+    print(b)
+
+## Reassignment
+c = True
+
+if c:
+    print(d)
+else:
+    print(b)
+
+## Looping
+for i in range(0, 10):
+    print(a)
+
+j = 0
+while j < 10:
+    print(b)
+    j += 1
+
+### Inline loop
+a = [i for i in range(0, 10)]
+print(a)
