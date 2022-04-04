11539676a503f51278f305e904459a40f8d274da
TommyGymer <tommygymer@gmail.com>
2022-04-05 00:00:49 +0100

    Arrays

--- py club.py
+++ py club.py
@@ -1,43 +1,54 @@
 # Getting started
 ## Basic functions
-print()
+# print()
 
-input()
+# input()
 
-### Use of the basic functions
-print(input(": "))
+# ### Use of the basic functions
+# print(input(": "))
 
-## Variables
-a = 5
-b = "string"
-c = False
+# ## Variables
+# a = 5
+# b = "string"
+# c = False
 
-print(a)
-d = input(f"{b}: ")
+# print(a)
+# d = input(f"{b}: ")
 
-## Branching
-if c:
-    print(d)
-else:
-    print(b)
+# ## Branching
+# if c:
+#     print(d)
+# else:
+#     print(b)
 
-## Reassignment
-c = True
+# ## Reassignment
+# c = True
 
-if c:
-    print(d)
-else:
-    print(b)
+# if c:
+#     print(d)
+# else:
+#     print(b)
 
-## Looping
-for i in range(0, 10):
-    print(a)
+# ## Looping
+# for i in range(0, 10):
+#     print(a)
 
-j = 0
-while j < 10:
-    print(b)
-    j += 1
+# j = 0
+# while j < 10:
+#     print(b)
+#     j += 1
 
-### Inline loop
-a = [i for i in range(0, 10)]
-print(a)
+# ### Inline loop
+# a = [i for i in range(0, 10)]
+# print(a)
+
+# print(chr(65))
+
+a = "stringz"
+
+for i in a:
+    n = ord(i)
+    n += 1
+    if n >= 123:
+        n -= 26
+    print(chr(n))
